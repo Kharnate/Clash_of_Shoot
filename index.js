3086ed7977c47ce1ca6e0ae68feb41bb3e9b132d
KPatel <KPatel@AUTO2019>
2022-06-08 15:00:38 -0400

    Using Div element instead of canvas.

--- index.js
+++ index.js
@@ -1,85 +1,32 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
+const game = () =>{
+    const parent = document.querySelector(".parent-div");
 
-canvas.style.width = '100vw';
-canvas.style.height = '100vh';
-
-canvas.width = canvas.offsetWidth;
-canvas.height = canvas.offsetHeight;
-
-var elemLeft = canvas.offsetLeft;
-var elemTop = canvas.offsetTop;
-
-const enemies = [];
-
-// class Enemy{
-//     constructor(x, y, radius, color){
-//         this.x = x;
-//         this.y = y;
-//         this.radius = radius;
-//         this.color = color;
-//     }
-
-//     draw(){
-//         ctx.beginPath();
-//         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-//         ctx.fillStyle = "transparent";
-//         ctx.strokeStyle = this.color;
-//         ctx.lineWidth = 3;
-//         ctx.stroke();
-//         ctx.fill();
-//     }
-// }
-
-function Enemy(x, y, radius, color){
-    var enemy = new Path2D();
-    enemy.arc(x, y, radius, 0, Math.PI*2);
-    ctx.fillStyle = "black";
-    ctx.strokeStyle = color;
-    ctx.lineWidth = 3;
-    return enemy;
-}
-
-function spawnEnemies(){
-    const x = canvas.width-120;
-    const y = canvas.height-160;
-    setInterval(()=>{
-        const randY = Math.random() * y + 60;
-        const randX = Math.random() * x + 60;
-        enemies.push(new Enemy(randX, randY, 30, "white"));
+    setInterval(function(){
+        addElement(parent);
     }, 1000);
 }
 
-function enemiesClicked(event){
-    var rect = canvas.getBoundingClientRect(),
-    x = event.clientX - rect.left, 
-    y = event.clientY - rect.top,
-    i;
+function addElement(parent){
+    const newDiv = document.createElement('div');
+    newDiv.style.borderRadius = "50%";
+    newDiv.style.height = "4em";
+    newDiv.style.width = "4em";
+    newDiv.style.border = "5px solid white";
+    newDiv.style.background = "transparent";
+    newDiv.style.position = "relative";
 
-    for(i = enemies.length-1; i>=0; --i){
-        if(ctx.isPointInPath(enemies[i], x, y, "nonzero")){
-            console.log("enemy killed")
-            enemies.splice(i,1);
-        }
-    }
+    var parentInfo = parent.getBoundingClientRect();
+    
+    var top = Math.floor(Math.random() * parentInfo.height) + 1;
+    var left = Math.floor(Math.random() * parentInfo.width) + 1;
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for(i=0; i<enemies.length; i++){
-        ctx.fill(enemies[i], "nonzero");
-        ctx.stroke(enemies[i], "nonzero");
-    }
+    newDiv.style.top = top + "px";
+    newDiv.style.left = left + "px";
+
+    parent.appendChild(newDiv);
+    console.log("count");
+
 }
 
-function animate(){
-    requestAnimationFrame(animate);
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    enemies.forEach((enemy) => {
-        ctx.stroke(enemy, "nonzero");
-        ctx.fill(enemy, "nonzero");
-    });
-}
 
-animate();
-spawnEnemies();
-canvas.addEventListener('click', enemiesClicked, false);
+game();
