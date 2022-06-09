e960af288596a7e3a27f691fc43a02fe0348cc3c
KPatel <KPatel@AUTO2019>
2022-06-09 10:59:05 -0400

    added click event, issues with div movements when clicking and spawing issue.

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
+const circles = [];
+
 const game = () =>{
     const parent = document.querySelector(".parent-div");
-
+    console.log(parent.getBoundingClientRect());
     setInterval(function(){
-        addElement(parent);
-    }, 1000);
+        createCircle(parent);
+    }, 500);
 }
 
-function addElement(parent){
-    const newDiv = document.createElement('div');
-    newDiv.style.borderRadius = "50%";
-    newDiv.style.height = "4em";
-    newDiv.style.width = "4em";
-    newDiv.style.border = "5px solid white";
-    newDiv.style.background = "transparent";
-    newDiv.style.position = "relative";
+function createCircle(parent){
+
+    const newCircle = document.createElement('div');
+    newCircle.style.borderRadius = "50%";
+    newCircle.style.height = "3em";
+    newCircle.style.width = "3em";
+    newCircle.style.border = ".25em solid white";
+    newCircle.style.background = "transparent";
+    newCircle.style.position = "relative";
+    newCircle.classList.add("new-circle");
 
     var parentInfo = parent.getBoundingClientRect();
-    
-    var top = Math.floor(Math.random() * parentInfo.height) + 1;
-    var left = Math.floor(Math.random() * parentInfo.width) + 1;
+    var top = Math.floor(Math.random() * (parent.offsetHeight - 20)) + 5;
+    var left = Math.floor(Math.random() * (parent.offsetWidth-20)) + 5;
+    console.log("Height: " + parent.clientHeight + " width: " + parent.offsetWidth)
+    newCircle.style.top = top + "px";
+    newCircle.style.left = left + "px";
+    newCircle.style.right = "10px;"
+    newCircle.style.bottom = "10px;"
+    newCircle.style.zIndex = (Math.random() * 999999)
 
-    newDiv.style.top = top + "px";
-    newDiv.style.left = left + "px";
-
-    parent.appendChild(newDiv);
-    console.log("count");
-
+    circles.push(newCircle);
+    parent.appendChild(newCircle);
 }
 
+document.addEventListener("click", function(){
+    for (let i = 0; i < circles.length; i++) {
+        circles[i].addEventListener('click', ()=>{
+            circles[i].remove();
+        })
+    }
+});
 
 game();
+
